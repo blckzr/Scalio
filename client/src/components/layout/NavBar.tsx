e0be6ab245b41254fe342186cbd9f30121964d60
Jan Kevin <jankevingerona15@gmail.com>
2026-01-16 03:23:21 +0800

    [style] updated the learn page

--- client/src/components/layout/NavBar.tsx
+++ client/src/components/layout/NavBar.tsx
@@ -55,15 +55,11 @@ const Navbar = () => {
       : "text-gray-500 hover:text-white px-5 py-2 font-eblack text-sm tracking-widest uppercase transition-colors";
 
   return (
-    <nav className="bg-transparent px-8 py-5 sticky top-0 z-50">
+    <nav className="bg-background px-8 py-5 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* --- LEFT: LOGO --- */}
         <Link to="/" className="flex items-center gap-3 group">
-          <img
-            src={logo}
-            alt="Logo"
-            className="h-10 w-auto object-contain transition-transform group-hover:scale-105"
-          />
+          <img src={logo} alt="Logo" className="h-10 w-auto object-contain" />
           <span className="text-2xl font-bold text-white tracking-tight">
             Scalio
           </span>
